7aeea9ee819de6dc0cf1cdd39ab9562f8315f947
freetimezp <kraftukrnet@ukr.net>
2022-08-07 18:24:13 +0300

    05.Js-game-01. Show timer.

--- public/assets/script.js
+++ public/assets/script.js
@@ -166,6 +166,10 @@ window.addEventListener('load', function () {
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
 
+            //timer
+            const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+            context.fillText('Timer: ' + formattedTime, 20, 100);
+
             //game over messages
             if(this.game.gameOver) {
                 context.textAlign = 'center';
@@ -208,9 +212,18 @@ window.addEventListener('load', function () {
             this.gameOver = false;
             this.score = 0;
             this.winningScore = 10;
+            this.gameTime = 0;
+            this.timeLimit = 5000;
         }
 
         update(deltaTime) {
+            if(!this.gameOver) {
+                this.gameTime += deltaTime;
+            }
+            if(this.gameTime > this.timeLimit) {
+                this.gameOver = true;
+            }
+
             this.player.update();
 
             if(this.ammoTimer > this.ammoInterval) {
@@ -235,7 +248,10 @@ window.addEventListener('load', function () {
 
                         if(enemy.lives <= 0) {
                             enemy.markedForDeletion = true;
-                            this.score += enemy.score;
+
+                            if(!this.gameOver) {
+                                this.score += enemy.score;
+                            }
 
                             if(this.score > this.winningScore) {
                                 this.gameOver = true;
